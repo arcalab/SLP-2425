cb866c836c646f01dc8fcc6478896da4957ed6a3
Renato Neves <nevrenato@gmail.com>
2025-02-22 16:13:08 +0000

    added denotational slides

--- big_step/MyLang.hs
+++ big_step/MyLang.hs
@@ -5,7 +5,7 @@ module MyLang where
 --------------------------------------------------
 
 -- Define a type for variables in our language.
-data Vars = X1 | X2 deriving Show
+data Vars = X1 | X2 deriving (Show,Eq)
 
 -- A State is represented as a function mapping variables to integer values.
 type State = Vars -> Int
@@ -22,15 +22,23 @@ data AExp
 -- e.g. 
 x1PlusOne = Sum (VAE X1) (I 1)
 x2PlusTwo = Sum (VAE X2) (I 2)
+tenPlusx2 = Sum (I 10) (VAE X2)
 more = Mult x1PlusOne x2PlusTwo
 -- ....
+neg :: AExp -> AExp
+neg e = Mult e (I (-1))
+
+sub :: AExp -> AExp -> AExp
+sub e1 e2 = Sum e1 (neg e2)
 
 -- Implement the semantics of arithmetic expressions
 -- you developed at home
 -- Given an arithmetic expression and a state, it evaluates to an integer.
 semAE :: AExp -> State -> Int
-semAE = undefined 
-
+semAE (VAE x) s = s x  
+semAE (I n) s = n 
+semAE (Sum e1 e2) s = (semAE e1 s) +  (semAE e2 s) 
+semAE (Mult e1 e2) s = (semAE e1 s) * (semAE e2 s) 
 --------------------------------------------------
 --------------------------------------------------
 --------------------------------------------------
@@ -59,18 +67,29 @@ data Prog
 -- Write down a few examples of simple programs 
 -- e.g. 
 x1PlusOne_Seq_x2PlusTwo = Seq (Asg X1 x1PlusOne) (Asg X2 x2PlusTwo)
-div = While Tt (Asg X1 x1PlusOne) 
+divergence = While Tt (Asg X1 x1PlusOne) 
 -- ....
 
 -- Define the semantics of boolean expressions.
 -- Given a boolean expression and a state, it evaluates to a boolean value.
 semBE :: BExp -> State -> Bool
-semBE = undefined
+semBE (LE e1 e2) s = (semAE e1 s) <= (semAE e1 s)
+semBE Tt s = True 
+semBE Ff s = False 
+semBE (Neg b) s = not (semBE b s)
+semBE (And b1 b2) s = (semBE b1 s) && (semBE b2 s) 
 
+
+chMem :: Vars -> Int -> State -> State
+chMem x v s = \y -> if x == y then v else s y
+ 
 -- Define the semantics of programs.
 -- Given a program and an initial state, it produces a new state after execution.
 semProg :: Prog -> State -> State
-semProg = undefined
+semProg (Asg x e) s = chMem x (semAE e s) s 
+semProg (Seq p1 p2) s = let s' = (semProg p1 s) in semProg p2 s' 
+semProg (Cond b p1 p2) s = if semBE b s then semProg p1 s else semProg p2 s 
+semProg (While b p) s = if semBE b s then semProg (While b p) (semProg p s) else s 
 
 --------------------------------------------------
 --------------------------------------------------
